c6fc358047bc315876abdd1df7c9b045ed212eac
tymzirak <100309103+tymzirak@users.noreply.github.com>
2022-03-21 02:57:59 +0200

    changed the placement of 'static' according to psr

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -5,7 +5,7 @@ namespace App\Core;
 
 abstract class Controller extends Database
 {
-    static public function view(string $viewName, array $viewData=[]): View
+    public static function view(string $viewName, array $viewData=[]): View
     {
         return new View($viewName, $viewData);
     }
